41c77d689f724fc02b5566351558540f49a28fbe
Zied REBHI <zrebhi@mantu.com>
2022-03-21 17:26:52 +0100

    Add project files.

---
+++ JWTWebApiCore/Controllers/UserController.cs
@@ -0,0 +1,71 @@
+﻿using JWTWebApiCore.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace JWTWebApiCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        [HttpGet("Admins")]
+        [Authorize(Roles = "Admin")]
+        public IActionResult AdminsEndpoint()
+        {
+            var currentUser = GetCurrentUser();
+
+            return Ok($"Hi {currentUser.GivenName}, you are an {currentUser.Role}");
+        }
+
+
+        [HttpGet("User")]
+        [Authorize(Roles = "User")]
+        public IActionResult UsersEndpoint()
+        {
+            var currentUser = GetCurrentUser();
+
+            return Ok($"Hi {currentUser.GivenName}, you are a {currentUser.Role}");
+        }
+
+        [HttpGet("AdminsAndUsers")]
+        [Authorize(Roles = "Admin,User")]
+        public IActionResult AdminsAndUsersEndpoint()
+        {
+            var currentUser = GetCurrentUser();
+
+            return Ok($"Hi {currentUser.GivenName}, you are an {currentUser.Role}");
+        }
+
+        [HttpGet("Public")]
+        public IActionResult Public()
+        {
+            return Ok("Hi, you're on public property");
+        }
+
+        private UserModel GetCurrentUser()
+        {
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+
+            if (identity != null)
+            {
+                var userClaims = identity.Claims;
+
+                return new UserModel
+                {
+                    Username = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.NameIdentifier)?.Value,
+                    EmailAddress = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Email)?.Value,
+                    GivenName = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.GivenName)?.Value,
+                    Surname = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Surname)?.Value,
+                    Role = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Role)?.Value
+                };
+            }
+            return null;
+        }
+    }
+}
---
+++ JWTWebApiCore/Models/UserConstants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWTWebApiCore.Models
+{
+    public class UserConstants
+    {
+        public static List<UserModel> Users = new List<UserModel>()
+        {
+            new UserModel() { Username = "zrebhi_admin", EmailAddress = "zrebhi_admin@mail.com", Password = "123", GivenName = "Zrebhi", Surname = "Z", Role = "Admin" },
+            new UserModel() { Username = "zrebhi_user", EmailAddress = "zrebhi_user@mail.com", Password = "123", GivenName = "Zrebhi 2", Surname = "Z", Role = "User" },
+        };
+    }
+}
